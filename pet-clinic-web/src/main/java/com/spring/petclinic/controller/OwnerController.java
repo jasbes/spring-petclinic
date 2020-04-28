340e672ee92acd89879539f7fce66acbf73f337d
Jefferson Silva <jsilva@pictet-technologies.com>
2020-04-28 13:37:25 +0200

    add vets and owners list

--- pet-clinic-web/src/main/java/com/spring/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/spring/petclinic/controller/OwnerController.java
@@ -1,8 +1,10 @@
 package com.spring.petclinic.controller;
 
+import com.spring.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author jsilva on 27/04/2020
@@ -11,8 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping(value = {"", "index", "index.html"})
-    public String index() {
-        return "owners/index";
+    public ModelAndView index(ModelAndView mv) {
+        mv.setViewName("owners/index");
+        mv.addObject("owners", ownerService.findAll());
+        return mv;
     }
 }
--- pet-clinic-web/src/main/java/com/spring/petclinic/controller/VetsController.java
+++ pet-clinic-web/src/main/java/com/spring/petclinic/controller/VetsController.java
@@ -1,8 +1,10 @@
 package com.spring.petclinic.controller;
 
+import com.spring.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author jsilva on 27/04/2020
@@ -11,8 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping(value = {"", "index", "index.html"})
-    public String index() {
-        return "vets/index";
+    public ModelAndView index(ModelAndView mv) {
+        mv.setViewName("vets/index");
+        mv.addObject("vets", vetService.findAll());
+        return mv;
     }
 }
